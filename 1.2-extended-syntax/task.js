4e2af7cc368ed47f8330d9a874fa5a3430652774
SvetlanaLyubimskaya <lyubimskaya.s@yandex.ru>
2020-10-13 12:06:03 +0300

    First commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,50 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    
+    'use strict';
+    let discriminant = Math.pow(b,2) - 4*a*c;
+    let x1 = - b / (2 * a);
+    let x2 = (- b + Math.sqrt(discriminant)) / (2 * a);
+    let x3 = (- b - Math.sqrt(discriminant)) / (2 * a);
+
+    if (discriminant < 0) {
+        return [];
+    } else if (discriminant == 0) {
+        return [x1];
+    } else if (discriminant > 0) {
+        return [x2,x3];
+    }
+
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+
+    if (marks.length == 0) {
+        return 0;
+    } else if (marks.length > 5) {
+        marks = marks.slice(0,5);
+        console.log('Аргументов больше 5, программа будет считать только первые 5');
+    } 
+
+    let sumMarks = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sumMarks = sumMarks + marks[i];        
+    }
+    
+    let averageMark = sumMarks / marks.length; 
+    return averageMark;
+    
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+
+    const year = new Date().getFullYear();
+    let age = year - dateOfBirthday.getFullYear();
+    
+    if ( age < 18 ) {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    } else if (age >= 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    }
+
 }
