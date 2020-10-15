046101aa1234f57db2b9a4c8dff29a714cb8f9f6
SvetlanaLyubimskaya <lyubimskaya.s@yandex.ru>
2020-10-15 12:57:56 +0300

    First commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,40 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+
+    if (Number.isNaN(percent) === false) {
+        percent = parseInt(percent, Number);
+    } else {
+        return `Параметр "Процентная ставка содержит неправильное значение ${percent}`
+    }
+
+    if (Number.isNaN(contribution) === false) {
+        contribution = parseInt(contribution, Number);
+    } else {
+        return `Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`
+    }
+
+    if (Number.isNaN(amount) === false) {
+        amount = parseInt(amount, Number);
+    } else {
+        return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`
+    }
+
+    let bodyCredit = amount - contribution; // тело кредита
+    let amountMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()); // количество выплачиваемых месяцев
+    let rate = percent / 12 / 100; // 1/12 процентной ставки / 100 
+    let payment = bodyCredit * (rate + rate / (((1 + rate) ** amountMonths) - 1)); // ежемес платеж
+    let totalAmount = payment * amountMonths ; // сумма, кот выплатит клиент
+   
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    if ((name == '') || (name == undefined) || (name === null) || (name === 'null') || (name === 'undefined') || (name === ' ')) {
+        name = 'Аноним';
+    } 
+
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    
+    return greeting; 
 }
